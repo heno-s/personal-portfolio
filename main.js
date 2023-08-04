0bc0d9f4d275857c91f52c9653f1dba2b01942d7
janko12 <henrich.sury97@gmail.com>
2023-08-04 17:56:23 +0200

    Style footer

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-import { github, facebook, linkedin, externalLink } from "./icons.js";
+import {
+    github,
+    facebook,
+    linkedin,
+    externalLink,
+    phone,
+    email,
+    house,
+} from "./icons.js";
 import createProject from "./project.js";
 import projects from "./projects.js";
 
@@ -12,6 +20,10 @@ const githubsDOM = document.querySelectorAll(".github");
 const facebooksDOM = document.querySelectorAll(".facebook");
 const linkedinsDOM = document.querySelectorAll(".linkedin");
 const externalLinksDOM = document.querySelectorAll(".external-link");
+
+document.querySelector(".phone").innerHTML = phone;
+document.querySelector(".email").innerHTML = email;
+document.querySelector(".address").innerHTML = house;
 
 githubsDOM.forEach((githubA) => {
     githubA.innerHTML = github;
