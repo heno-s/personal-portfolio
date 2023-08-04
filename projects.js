ec9fd9b77e206a89f1667b36436ec123419f3dc6
janko12 <henrich.sury97@gmail.com>
2023-08-04 17:03:10 +0200

    Add all projects

--- projects.js
+++ projects.js
@@ -1,5 +1,23 @@
 export default [
     {
+        title: "Only Members",
+        description:
+            "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate, placeat!",
+        img: "./projects-images/only-members.png",
+        repository: "https://github.com/heno-s/only-members",
+        live: "https://only-members.onrender.com/",
+        tags: ["Express", "NodeJs", "MongoDB"],
+    },
+    {
+        title: "Memory Game",
+        description:
+            "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate, placeat!",
+        img: "./projects-images/memory-game.png",
+        repository: "https://github.com/heno-s/memory-card",
+        live: "https://heno-s.github.io/memory-card/",
+        tags: ["ReactJs"],
+    },
+    {
         title: "Top Shop",
         description:
             "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate, placeat!",
@@ -7,6 +25,42 @@ export default [
         repository:
             "https://github.com/heno-s/odin-shopping-cart-vite",
         live: "https://topshop-odin.vercel.app/shop",
-        tags: ["ReactJs", "Vite", "NodeJs"],
+        tags: ["ReactJs", "Vite"],
+    },
+    {
+        title: "Weather App",
+        description:
+            "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate, placeat!",
+        img: "./projects-images/weather.png",
+        repository: "https://github.com/heno-s/odin-weather-app",
+        live: "https://heno-s.github.io/odin-weather-app/",
+        tags: ["VanillasJs"],
+    },
+    {
+        title: "Todo Manager",
+        description:
+            "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate, placeat!",
+        img: "./projects-images/todo.png",
+        repository: "https://github.com/heno-s/odin-todo-manager-ts",
+        live: "https://heno-s.github.io/odin-todo-manager-ts/",
+        tags: ["Typescript"],
+    },
+    {
+        title: "Tic Tac Toe",
+        description:
+            "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate, placeat!",
+        img: "./projects-images/tic-tac-toe.png",
+        repository: "https://github.com/heno-s/odin-tic-tac-toe",
+        live: "https://heno-s.github.io/odin-tic-tac-toe/",
+        tags: ["VanillaJs"],
+    },
+    {
+        title: "Library",
+        description:
+            "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate, placeat!",
+        img: "./projects-images/library.png",
+        repository: "https://github.com/heno-s/revisioned-library",
+        live: "https://heno-s.github.io/revisioned-library/",
+        tags: ["ReactJs"],
     },
 ];
